6b4f18c01fd06f1111295ae98c7db0b5d30b0fe9
unknown <montinformatic233@gmail.com>
2023-03-22 07:34:20 -0500

    Mi portafolio personal

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hola Mundo");
+let nav=false;
+function viewNav(){
+    if(nav){
+        document.getElementById('nav__responsive').style.transform="translateX(100%)"
+        nav=false;
+    }else{
+        document.getElementById('nav__responsive').style.transform="translateX(0%)"
+        nav=true;
+    }
+}
+
+document.addEventListener('scroll', ()=>{
+    const nav=document.querySelector('nav');
+    if(window.scrollY>100){
+        nav.classList.add('scrolled');
+    }else{
+        nav.classList.remove('scrolled');
+    }
+})
